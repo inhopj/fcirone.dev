ca1e5d0db860091fff2bbb1339963694ddf6f8a5
fra <fra.cirone@gmail.com>
2022-04-08 18:02:29 +0200

    cleanup Header comp

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -54,17 +54,6 @@ const Header = ({ className: parentClassNames }: Props) => {
           path={feedsPath}
           title="Feeds"
         />
-        {/* <a
-          href={feedsPath}
-          className={classnames(
-            parentClassNames,
-            true
-              ? 'font-semibold text-zinc-800 dark:text-zinc-200'
-              : 'font-normal text-zinc-600 dark:text-zinc-400',
-            'rounded-lg cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-800 p-3 text-gray-600 dark:text-gray-400'
-          )}>
-          Feeds
-        </a> */}
       </div>
       {mounted && <button
         className="bg-zinc-200 dark:bg-zinc-800 p-3 rounded-lg cursor-pointer"
