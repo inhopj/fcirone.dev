f6372bd6720299d58c2755181461354e7d35573b
fra <fra.cirone@gmail.com>
2022-05-12 16:42:12 +0200

    Add error card in case of empty array of Feeds

--- components/Feeds/Feeds.tsx
+++ components/Feeds/Feeds.tsx
@@ -18,56 +18,66 @@ const Feeds = ({
   items
 }: Props) => {
 
-  const [tenRandom, setTenRandom] = useState<Feed[]>([])
-  useEffect(() => {
-
-    const tenRandomFeeds: Feed[] = items?.sort(() => 0.5 - Math.random()).slice(0, 10)
-    console.log("tenRandomFeeds ", tenRandomFeeds)
-    setTenRandom(tenRandomFeeds)
-  }, [items]
-  )
+  // Sufficiently random since not so many items (thirty Feeds returned by EchoJs)
+  const tenRandomFeeds: Feed[] = items?.sort(() => 0.5 - Math.random()).slice(0, 10)
 
   const getSubtitle = (url: string) => {
     return new URL(url).origin.split('://')[1]
   }
 
   return (
-    <div
-      className={classnames(
-        'flex flex-col max-w-2xl w-full md:pl-3',
-        parentClassNames
-      )}
-    >
-      <h1 id="feeds" className="text-2xl md:text-4xl text-black dark:text-white mb-3">
-        Feeds
-      </h1>
-      {tenRandom.map((item, index) => {
-        const last = index === tenRandom.length - 1
-        const subtitle = item.guid ? `at ${getSubtitle(item.guid)}` : ''
-        console.log("ITEM ", item)
-        return (
-          // eslint-disable-next-line react/jsx-no-target-blank
-          <a
-            key={index}
-            href={item.guid}
-            target='_blank'
-          >
-            <Card
-              key={index}
-              className={classnames(
-                'text-zinc-600 dark:text-zinc-400',
-                {
-                  'mb-3': !last
-                },
-              )}
-              title={item.title}
-              subtitle={subtitle}
-              href={item.guid}
-            />
-          </a>
-        )
-      })}
-    </div>
+    <>
+      <div
+        className='flex justify-start max-w-2xl w-full md:pl-3'>
+        <h1 id="feeds" className="text-2xl md:text-4xl text-black dark:text-white mb-3">
+          Feeds
+        </h1>
+      </div>
+      {items.length > 0 &&
+        <div
+          className={classnames(
+            'flex flex-col max-w-2xl w-full md:pl-3',
+            parentClassNames
+          )}
+        >
+
+          {tenRandomFeeds.map((item, index) => {
+
+            const last = index === tenRandomFeeds.length - 1
+            const subtitle = item.guid ? `at ${getSubtitle(item.guid)}` : ''
+            return (
+              // eslint-disable-next-line react/jsx-no-target-blank
+              <a
+                key={index}
+                href={item.guid}
+                target='_blank'
+              >
+                <Card
+                  key={index}
+                  className={classnames(
+                    'text-zinc-600 dark:text-zinc-400',
+                    {
+                      'mb-3': !last
+                    },
+                  )}
+                  title={item.title}
+                  subtitle={subtitle}
+                  href={item.guid}
+                />
+              </a>
+            )
+          })}
+        </div>
+      }
+      {items.length == 0 &&
+        <Card
+          className='text-zinc-600 dark:text-zinc-400 w-6/12 p-5'
+          centerText
+          title='Oops!'
+          subtitle='Something went wrong retriving feeds'
+        />
+      }
+    </>
   )
 }
 
