238409d8be39c33e941e151838b6de1c1baf5aca
inhopj <fra.cirone@gmail.com>
2022-05-13 15:23:07 +0200

    remove random(echoJS) feeds from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
-import type { GetStaticProps, NextPage } from 'next'
-import Parser from 'rss-parser'
+import type { NextPage } from 'next'
 import { Feed } from '../components/Feeds/Feeds'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
-const Feeds = dynamic(
-  () => import('../components/Feeds/Feeds'),
-  { ssr: false }
-)
 const Header = dynamic(
   () => import('../components/Header/Header'),
   { ssr: false }
@@ -17,11 +12,7 @@ const Hero = dynamic(
   { ssr: false }
 )
 
-interface Props {
-  feeds: Feed[]
-}
-
-const Home: NextPage<Props> = ({ feeds }) => {
+const Home: NextPage = () => {
 
   return (
     <>
@@ -38,43 +29,9 @@ const Home: NextPage<Props> = ({ feeds }) => {
       >
         <Header className='mb-7' />
         <Hero className='mb-6' />
-        <Feeds
-          className=''
-          items={feeds}
-        />
       </div>
     </>
   )
-}
-
-const loadFeeds = async (): Promise<Feed[]> => {
-
-  try {
-    //EchoJs RSS feed url TODO - move in constants file
-    const RSS_URL = 'https://www.echojs.com/rss'
-
-    let parser = new Parser();
-
-    const feeds = await parser.parseURL(RSS_URL)
-
-    return feeds.items as Feed[]
-
-  } catch (error) {
-    console.log(error)
-    return [] as Feed[]
-  }
-}
-
-// Runs only on the server side
-export const getStaticProps: GetStaticProps = async () => {
-  // Instead of fetching your `/api` route you can call the same function directly in `getStaticProps`
-  const feeds = await loadFeeds()
-
-  // Props returned will be passed to the page component
-  return {
-    props: { feeds },
-    revalidate: 60,
-  }
 }
 
 export default Home
