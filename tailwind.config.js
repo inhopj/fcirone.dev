cbdd35d7fcec652c311c8aa06e3ec8d148de4d31
inhopj <fra.cirone@gmail.com>
2022-05-16 15:25:36 +0200

    add transitionDuration in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        '0': '0ms',
+        '100': '100ms',
+        '200': '200ms',
+        '400': '400ms',
+        '500': '500ms',
+        '600': '600ms',
+        '700': '700ms',
+        '1000': '1000ms',
+        '2000': '2000ms'
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
