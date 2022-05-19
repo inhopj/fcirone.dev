bf111e7a828de38a32a68d5936008baf8fb0eb65
inhopj <fra.cirone@gmail.com>
2022-05-19 09:11:44 +0200

    cleanup Bio

--- components/About/Bio.tsx
+++ components/About/Bio.tsx
@@ -1,19 +1,11 @@
-import Image from 'next/image'
-
-interface Props {
-  className?: String;
-}
-
-// TODO - cleanup and add parentClassName
-const Bio = ({ className: parentClassNames }: Props) => {
-
+const Bio = () => {
   return (
     <div className='pb-4'>
       <div className='text-white text-xl md:text-2xl'>
         Bio
       </div>
       <p>
-        Nintendo person! into outdoors, board sports &#128761; &#32;and foreign languages<br />(English, French, Spanish and a bit of Maroccan)
+        Nintendo person! into outdoors &#127794; &#32;, board sports &#128761; &#32;and foreign languages<br />(English, French, Spanish and a bit of Maroccan)
       </p>
     </div>
 
