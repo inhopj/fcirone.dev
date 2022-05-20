945ecce734a85b3e679288610f22de8cb5df3185
inhopj <fra.cirone@gmail.com>
2022-05-20 17:06:29 +0200

    use next/link to test prefetching in prod(it works in prod only by default)

--- components/Link/Anchor.tsx
+++ components/Link/Anchor.tsx
@@ -1,4 +1,5 @@
 import classnames from 'classnames'
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 interface Props {
@@ -14,22 +15,36 @@ const Anchor = ({
 }: Props) => {
 
   const router = useRouter();
-  
+
   const isActive = router.asPath === path
 
   return (
     // eslint-disable-next-line @next/next/no-html-link-for-pages
-    <a
-      href={path}
-      className={classnames(
-        parentClassNames,
-        isActive
-          ? 'text-white'
-          : 'text-zinc-400',
-        'rounded-lg cursor-pointer px-3 hover:text-yellow-300'
-      )}>
-      {title}
-    </a>
+    // <a
+    //   href={path}
+    //   className={classnames(
+    //     parentClassNames,
+    //     isActive
+    //       ? 'text-white'
+    //       : 'text-zinc-400',
+    //     'rounded-lg cursor-pointer px-3 hover:text-yellow-300'
+    //   )}>
+    //   {title}
+    // </a>
+
+    <Link href={path}>
+      <a
+        href={path}
+        className={classnames(
+          parentClassNames,
+          isActive
+            ? 'text-white'
+            : 'text-zinc-400',
+          'rounded-lg cursor-pointer px-3 hover:text-yellow-300'
+        )}>
+        {title}
+      </a>
+    </Link>
   )
 }
 
