d2142b92f5248ed5503f66d9dbe0aa6e140ef317
inhopj <fra.cirone@gmail.com>
2022-05-23 10:57:41 +0200

    add contacts section in About component

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,15 +1,16 @@
 import classnames from 'classnames'
 import Image from 'next/image'
+import Github from '../Icons/Github';
 import Bio from './Bio';
 import Tech from './Tech';
 import Work from './Work';
 
+// TODO - centralize this infinitely duplicated interface
 interface Props {
   className?: string;
 }
 
-const Cotacts = ({ className: parentClassNames }: Props) => {
-
+const About = ({ className: parentClassNames }: Props) => {
   return (
     <div
       className={classnames(
@@ -18,12 +19,36 @@ const Cotacts = ({ className: parentClassNames }: Props) => {
       )}
     >
       <div className='flex flex-col justify-start text-zinc-400'>
-        <Work className='mb-6'/>
-        <Tech className='mb-6'/>
-        <Bio />
+        <Work className='mb-6' />
+        <Tech className='mb-6' />
+        <Bio className='mb-6' />
+
+        {/* CONTACTS */}
+        <div className='flex justify-center'>
+          <div className='flex mr-4 cursor-pointer'>
+            <Github className='fill-white w-8' />
+          </div>
+          <div className='flex mr-4 cursor-pointer'>
+            <Image
+              src='/linkedin.svg'
+              alt="logo-icon"
+              width={30}
+              height={30}
+              className="rounded"
+            />
+          </div>
+          <div className='flex mr-3 cursor-pointer'>
+            <Image
+              src='/mail.svg'
+              alt="logo-icon"
+              width={35}
+              height={35}
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Cotacts;
+export default About;
