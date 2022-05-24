de88e4ea22c3deba53e3fc7da1c1b12f3c1eab8e
inhopj <fra.cirone@gmail.com>
2022-05-24 16:01:39 +0200

    remove animation stuff from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { motion } from 'framer-motion';
 
-// TODO - cleanup animation stuff and remove framer-motion dep from package.json
-function MyApp({ Component, pageProps, router }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <motion.div
-    //   key={router.route}
-    //   initial="initial"
-    //   animate="animate"
-    //   variants={{
-    //     initial: {
-    //       opacity: 1,
-    //     },
-    //     animate: {
-    //       opacity: 0.97,
-    //     }
-    //   }}
-    // >
-    //   <Component {...pageProps} />
-    // </motion.div>
     <Component {...pageProps} />
   )
 }
