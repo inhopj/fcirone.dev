9ed2f747c10c2bad52bd80ec78b94ce691a8609d
inhopj <fra.cirone@gmail.com>
2022-05-28 10:23:54 +0200

    add TODO in _app root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// TODO - consider using a shared page Layout for the header and the page skeleton
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Component {...pageProps} />
