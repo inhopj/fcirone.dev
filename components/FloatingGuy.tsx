4c0da712ec9716bda2251f2bed9e5636d5606886
inhopj <fra.cirone@gmail.com>
2022-05-29 22:58:48 +0200

    refactor FloatingGuy

--- components/FloatingGuy.tsx
+++ components/FloatingGuy.tsx
@@ -1,31 +1,48 @@
 import Javascript from './Icons/Javascript';
 import Nodejs from './Icons/Nodejs';
 import React from '../components/Icons/React'
+import { useState } from 'react';
+import Anchor from './Link/Anchor';
+import classnames from 'classnames';
+import Image from 'next/image';
+
+const anchors = [
+  { href: '#react', icon: <React className='w-8' />, parentClassNames: "flex mb-4" },
+  { href: '#node', icon: <Nodejs className='w-9' />, parentClassNames: "flex mb-4" },
+  { href: '#javascript', icon: <Javascript className='w-8 rounded-lg' />, parentClassNames: "flex mb-4" },
+]
+
 
 const FloatingGuy = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div
-        className='flex flex-col items-center fixed bottom-5 right-5 md:bottom-10 md:right-10'
+      className='flex flex-col-reverse items-center fixed bottom-5 right-5 md:bottom-10 md:right-10'
+    >
+      <div
+        className='flex justify-center items-center w-9 h-9 rounded-lg bg-[#f0db4f] text-lg'
+        onClick={() => setToggle(!toggle)}
       >
-        <a
-          href="#javascript"
-          className='flex mb-4 md:mb-3'
-        >
-          <Javascript className='w-9 rounded-lg' />
-        </a>
-        <a
-          href="#node"
-          className='flex mb-3'
-        >
-          <Nodejs className='w-10' />
-        </a>
-        <a
-          href="#react"
-          className='flex'
-        >
-          <React className='w-9' />
-        </a>
+        #
       </div>
+      {anchors.map(({ href, icon, parentClassNames }, index) => {
+        return (
+          <div
+            key={href}
+            className={classnames(
+              parentClassNames
+            )}
+          >
+            <a
+              href={href}
+            >
+              {icon}
+            </a>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
