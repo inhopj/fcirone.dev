1ca77ba6c4dd399111ffc0f77000e703b0006cc8
inhopj <fra.cirone@gmail.com>
2022-05-30 15:34:05 +0200

    create useOutsideClick hook

---
+++ hooks/useOutsideClick.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export const useOutsideClick = (callback: () => void) => {
+  const ref = useRef<Node>();
+
+  useEffect(() => {
+    const handleClick = (event: Event) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [ref]);
+
+  return ref;
+};
