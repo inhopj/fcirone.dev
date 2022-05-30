56c0def3dbaa95aa06fd30177fd61bd87850b483
inhopj <fra.cirone@gmail.com>
2022-05-30 17:14:00 +0200

    add script to generate feeds

---
+++ scripts/rss.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs')
+const RSS =  require('rss')
+
+let feeds = JSON.parse(fs.readFileSync(`${process.cwd()}/public/feeds.json`))
+
+const generateRss = () => {
+  const rss = new RSS({
+    title: 'Fracensco Cirone',
+    site_url: 'https://fcirone.dev',
+    feed_url: 'https://fcirone.dev/rss.xml'
+  });
+  const { javascript, node, react } = feeds
+  const allFeeds = javascript.concat(node).concat(react)
+  allFeeds.map((feed) => {
+    rss.item({
+      title: feed.title,
+      url: feed.link,
+    });
+  });
+
+  fs.writeFileSync(`${process.cwd()}/public/rss.xml`, rss.xml({ indent: true }));
+}
+
+generateRss()
