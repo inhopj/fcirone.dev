a5c208c497f8a472fb0342366669d915f8b685ee
inhopj <fra.cirone@gmail.com>
2022-06-03 17:52:43 +0200

    some text indentation in Bio comp

--- components/About/Bio.tsx
+++ components/About/Bio.tsx
@@ -11,10 +11,12 @@ const Bio = ({ className: parentClassNames }: Props) => {
         Bio
       </div>
       <p>
-        Nintendo person! Into outdoors &#127794; &#32;, board sports &#128761; &#32;<br />and foreign languages<br />(🇬🇧, 🇫🇷, 🇪🇸 and a bit of 🇲🇦)
+        Nintendo person!<br />
+        Into outdoors &#127794; &#32;, board sports &#128761; &#32;<br />
+        and foreign languages<br />
+        🇬🇧 🇫🇷 🇪🇸 
       </p>
     </div>
-
   )
 }
 
