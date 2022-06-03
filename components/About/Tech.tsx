0ecbeb161689dc0027506bbc286d0c8d62c928fc
inhopj <fra.cirone@gmail.com>
2022-06-03 17:05:05 +0200

    more of a Tech section than a Skills one

--- components/About/Tech.tsx
+++ components/About/Tech.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-no-target-blank */
 import classnames from 'classnames';
-import Image from 'next/image'
 
 interface Props {
   className?: string;
@@ -10,42 +9,14 @@ const Tech = ({ className: parentClassNames }: Props) => {
   return (
     <div className={classnames(parentClassNames)}>
       <p className='text-white text-xl md:text-2xl'>
-        Skills
+        Tech
       </p>
-      <p className='mb-3'>
-        I love using
-        <span
-          className='inline-flex items-center align-bottom pr-1'
-        >
-          <a
-            href="https://nodejs.org"
-            target="_blank"
-            rel="noopener"
-            className='pl-1 cursor-pointer hover:text-white'
-          >
-            Node
-          </a>
-          {/* TODO - create Node-svg component */}
-          <Image src="/node-js.svg" alt="Node" width={20} height={20} />
-        </span>
-        <span>
-          and
-        </span>
-        <span
-          className='inline-flex items-center align-bottom pl-1 pr-1'
-          >
-          {/* TODO - create React-svg component */}
-          <Image src="/react.svg" alt="React" width={20} height={20} />
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener"
-            className='cursor-pointer hover:text-white'
-          >
-            React
-          </a>
-        </span>
-        to <br />build web things.<br />Check out my Feeds for related contents!
+      <p>
+        I love using Node and React to<br />
+        build up-to-date web things.<br />
+        Check out the Feeds page for a collection<br />
+        of interesting news and articles<br />
+        found here and there =)
       </p>
     </div>
   )
