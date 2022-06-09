a2cb2472fb407505e60754f3826357ab962e935b
inhopj <fra.cirone@gmail.com>
2022-06-09 18:59:58 +0200

    create tailwind component

--- components/Icons/Tailwind.tsx
+++ components/Icons/Tailwind.tsx
@@ -2,19 +2,21 @@ import classNames from "classnames";
 
 interface Props {
   className?: string;
+  size: number;
+  fill: string
 }
 
-const Tailwind = ({ className: parentClassNames }: Props) => {
+const Tailwind = ({ className: parentClassNames, size, fill }: Props) => {
   return (
     <svg
     className={classNames(parentClassNames)}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
     >
       <path
-        fill="white"
+        fill={fill}
         d="M18.5 9.51a4.22 4.22 0 0 1-1.91-1.34A5.77 5.77 0 0 0 12 6a4.72 4.72 0 0 0-5 4 3.23 3.23 0 0 1 3.5-1.49 4.32 4.32 0 0 1 1.91 1.35A5.77 5.77 0 0 0 17 12a4.72 4.72 0 0 0 5-4 3.2 3.2 0 0 1-3.5 1.51zm-13 4.98a4.22 4.22 0 0 1 1.91 1.34A5.77 5.77 0 0 0 12 18a4.72 4.72 0 0 0 5-4 3.23 3.23 0 0 1-3.5 1.49 4.32 4.32 0 0 1-1.91-1.35A5.8 5.8 0 0 0 7 12a4.72 4.72 0 0 0-5 4 3.2 3.2 0 0 1 3.5-1.51z"
       />
     </svg>
