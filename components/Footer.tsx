768e4c2d44adc1455fcd992b9c35d30305ac656e
inhopj <fra.cirone@gmail.com>
2022-06-13 14:16:33 +0200

    use Triangle in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-no-target-blank */
 import classnames from 'classnames'
 import Image from 'next/image'
-import { LinkedinLogo, PhosphorLogo, Triangle } from 'phosphor-react';
+import { LinkedinLogo, PhosphorLogo } from 'phosphor-react';
 import Github from './Icons/Github';
 import Tailwind from './Icons/Tailwind';
+import Triangle from './Icons/Triangle';
 
 interface Props {
   className?: string;
@@ -26,44 +27,42 @@ const Footer = ({ className: parentClassNames }: Props) => {
       </div>
 
       <div
-        className='flex max-w-2xl w-full items-center pt-2 md:pt-2'
+        className='flex max-w-2xl w-full justify-center items-center pt-2 md:pt-2'
       >
-        <div className='flex flex-col w-full'>
-          <div className="flex justify-start justify-center">
-            <a
-              className='nextjs flex mr-4'
-              href='https://nextjs.org/'
-              target='_blank'
-            >
-              <Image
-                src='/nextjs-icon.svg'
-                alt="next-logo-icon"
-                width={28}
-                height={28}
-              />
-            </a>
-            <a
-              className='tailwind flex mr-4'
-              href='https://tailwindcss.com/'
-              target='_blank'
-            >
-              <Tailwind size={28} fill='white'/>
-            </a>
-            <a
-              className='flex items-baseline mr-4 pt-1'
-              href='https://vercel.com/'
-              target='_blank'
-            >
-              <Triangle size={25} color="#ffffff" weight="fill" />
-            </a>
-            <a
-              className='flex items-center md:pb-1'
-              href='https://phosphoricons.com/'
-              target='_blank'
-            >
-              <PhosphorLogo size={25} color="#ffffff" weight="duotone" />
-            </a>
-          </div>
+        <div className="flex justify-start items-center">
+          <a
+            className='nextjs flex mr-4'
+            href='https://nextjs.org/'
+            target='_blank'
+          >
+            <Image
+              src='/nextjs-icon.svg'
+              alt="next-logo-icon"
+              width={28}
+              height={28}
+            />
+          </a>
+          <a
+            className='tailwind flex mr-4'
+            href='https://tailwindcss.com/'
+            target='_blank'
+          >
+            <Tailwind size={28} fill='white' />
+          </a>
+          <a
+            className='flex items-baseline mr-4'
+            href='https://vercel.com/'
+            target='_blank'
+          >
+            <Triangle className='w-5' size={20} fill="#ffffff" />
+          </a>
+          <a
+            className='flex items-center md:pb-1'
+            href='https://phosphoricons.com/'
+            target='_blank'
+          >
+            <PhosphorLogo size={25} color="#ffffff" weight="duotone" />
+          </a>
         </div>
       </div>
     </>
